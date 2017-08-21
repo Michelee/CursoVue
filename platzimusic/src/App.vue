a2562e371d42b6a57fe14089af926bcf462de258
Michele Ancheta <michelee.ancheta@gmail.com>
2017-08-21 18:20:20 -0400

    Configurando la API de Lastfm

---
+++ platzimusic/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="artist in artists">
+        {{ artist.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import getArtists from './api'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'PlatziMusic',
+      artists: []
+    }
+  },
+  mounted: function(){
+    const self = this
+    getArtists()
+      .then(function (artists) {
+          self.artists = artists
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,7 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: '236846376780c71dd2b56c51048fdf9e',
+	secret: '733c02939296f9085d35c177c822ecd3',
+	registeredTo: 'micheleancheta'
+}
+
